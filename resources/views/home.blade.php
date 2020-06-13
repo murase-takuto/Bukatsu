4194c3eaaa3eace59e18721cf4f6815586495cb9
murase-takuto <takumura0911@gmail.com>
2020-06-13 19:38:38 +0900

    [add]permission確認部分の追加
    home.bladeにログイン中ユーザーの権限を確認できるviewを追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,32 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <p>
+                        @can('admin_permission')
+                            admin_permission を持っています <br>
+                        @endcan
 
-                    You are logged in!
+                        @can('manager_permission')
+                            manager_permission を持っています <br>
+                        @endcan
+
+                        @can('player_permission')
+                         player_permission を持っています <br>
+                        @endcan
+                    </p>
+                    <p>
+                        @role('admin')
+                            あなたのロールは admin です<br>
+                        @endrole                    
+
+                        @role('manager')
+                            あなたのロールは manager です<br>
+                        @endrole                    
+
+                        @role('player')
+                            あなたのロールは player です<br>
+                        @endrole                    
+                    </p>
                 </div>
             </div>
         </div>
